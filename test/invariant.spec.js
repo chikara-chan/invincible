00acac55888ba827d55853eef100cb7c208b2941
chikara <chenhongtu@51xianqu.net>
2017-02-15 20:05:49 +0800

    Update test

--- test/invariant.spec.js
+++ test/invariant.spec.js
@@ -3,13 +3,21 @@ import invariant from '../src/invariant'
 
 describe('invariant', () => {
   it('should throw when `message` argument is undefined', () => {
-    expect(() => {invariant(false)}).to.throw(Error)
-    expect(() => {invariant(true)}).to.throw(Error)
+    expect(() => {
+      invariant(false)
+    }).to.throw(Error)
+    expect(() => {
+      invariant(true)
+    }).to.throw(Error)
   })
   it('should throw when `condition` argument is false', () => {
-    expect(() => {invariant(false, 'something wrong')}).to.throw(Error)
+    expect(() => {
+      invariant(false, 'something wrong')
+    }).to.throw(Error)
   })
   it('should not throw when `condition` argument is true', () => {
-    expect(() => {invariant(true, 'something wrong')}).to.not.throw(Error)
+    expect(() => {
+      invariant(true, 'something wrong')
+    }).to.not.throw(Error)
   })
 })
