8125547d397e15515c77ed3b0ef5811c55191088
chikara <chenhongtu@51xianqu.net>
2017-02-15 20:08:22 +0800

    Update webpack plugin system

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,13 +1,12 @@
 const webpack = require('webpack'),
   path = require('path'),
+  NODE_ENV = process.env.NODE_ENV,
   config = {
     context: path.resolve(__dirname, '..'),
-    entry: {
-      invincible: './src/index'
-    },
+    entry: {invincible: './src/index'},
     output: {
       path: './dist',
-      filename: '[name].js',
+      filename: NODE_ENV === 'production' ? '[name].min.js' : '[name].js',
       library: 'Invincible',
       libraryTarget: 'umd'
     },
@@ -16,24 +15,21 @@ const webpack = require('webpack'),
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel',
-        query: {
-          cacheDirectory: true
-        }
+        query: {cacheDirectory: true}
       }]
     },
-    resolve: {
-      extensions: ['', '.js', '.json']
-    },
+    resolve: {extensions: ['', '.js', '.json']},
     plugins: [
       new webpack.optimize.OccurrenceOrderPlugin(),
-      new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        },
-        comments: false
-      })
+      new webpack.optimize.DedupePlugin()
     ]
   }
+
+if (NODE_ENV === 'production') {
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {warnings: false},
+    comments: false
+  }))
+}
 
 module.exports = config
